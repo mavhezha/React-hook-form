1cd7848c625435c042bd8b75de8216da22b9a7f3
Arnold P Mavhezha <arnoemavhezha@gmail.com>
2023-04-10 08:19:08 +0000

    Update 5 files

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -1,22 +1,26 @@
-import React from "react";
+import { useForm } from "react-hook-form";
+import { DevTool }  from "@hookform/devtools";
 
 function YouTubeForm() {
+  const form = useForm();
+  const { register, control } = form;n
   return (
-    <div>
+    <div>h1/>/>
       <form>
         <label htmlFor="username">Username</label>
-        <input type="text" id="username" name="username" />
+        <input type="text" id="username" {...register("username")} />
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" name="email" />
+        <input type="email" id="email" {...register("email")} />
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" name="channel" />
+        <input type="text" id="channel" {...register("channel")} />
 
         <button>Submit</button>
       </form>
+      <DevTool control={control} />e />
     </div>
   );
-}
+};
 
 export default YouTubeForm;
