fc04019104edf20829bd3572c175d94fd529845b
Arnold P Mavhezha <arnoemavhezha@gmail.com>
2023-04-12 08:21:21 +0000

    Error message display updated

--- src/components/YouTubeForm.tsx
+++ src/components/YouTubeForm.tsx
@@ -9,10 +9,11 @@ type FormValues = {
 
 function YouTubeForm() {
   const form = useForm<FormValues>();
-  const { register, control, handleSubmit } = form;
+  const { register, control, handleSubmit, formState } = form;
+  const { errors } = formState;
   
   const onSubmit = (data: FormValues) => {
-    console.log("Form submitted", data);c
+    console.log("Form submitted", data);
   }; 
   
   return (
@@ -24,15 +25,33 @@ function YouTubeForm() {
           id="username" 
           {...register("username", {
             required: "Username is required", 
-          
-            
-          })} />
+          })} 
+        />
+        <p className="error">{errors.username?.message}</p>
 
         <label htmlFor="email">Email</label>
-        <input type="email" id="email" {...register("email")} />
+        <input 
+          type="email" 
+          id="email" 
+          {...register('email', {
+            required: 'Email is required',
+            pattern: {
+              value: /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+              message: 'Please enter a valid email',
+            },
+          })} 
+         />
+         <p className="error">{errors.email?.message}</p>
 
         <label htmlFor="channel">Channel</label>
-        <input type="text" id="channel" {...register("channel")} />
+        <input 
+          type="text" 
+          id="channel" 
+          {...register("channel", {
+            required: "Channel is required", 
+          })} 
+         />
+         <p className="error">{errors.channel?.message}</p>
 
         <button>Submit</button>
       </form>
